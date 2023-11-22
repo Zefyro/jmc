64b25256e946015dd2347b432a6e9d7232a426fa
Zefyro <71092443+Zefyro@users.noreply.github.com>
2023-11-22 22:15:23 +0200

    Remove unnecessary comments

--- JMC.Terminal/Configuration.cs
+++ JMC.Terminal/Configuration.cs
@@ -39,11 +39,9 @@ public class Configuration
     /// <param name="args">Command-line arguments passed to the application.</param>
     internal void Load(string[] args)
     {
-        // Check if the configuration file exists
         if (!File.Exists(FileName))
             Initialize(args);
 
-        // Read the contents of the configuration file
         using StreamReader sr = new(FileName);
         Configuration config = JsonSerializer.Deserialize<Configuration>(sr.ReadToEnd())!;
 
@@ -54,7 +52,6 @@ public class Configuration
         Target = config.Target;
         Output = config.Output;
 
-        // Mark that the configuration has been loaded
         HasConfig = true;
     }
 
@@ -63,10 +60,7 @@ public class Configuration
     /// </summary>
     internal void Save()
     {
-        // Serialize the configuration object to JSON
         string config = JsonSerializer.Serialize(this, WriteOptions);
-
-        // Write the serialized JSON to the configuration file
         File.WriteAllText(FileName, config);
     }
 
@@ -77,25 +71,20 @@ public class Configuration
     /// <param name="args">Command-line arguments passed to the application.</param>
     internal void Initialize(string[] args)
     {
-        // Check if the configuration file already exists
         if (File.Exists(FileName))
         {
             Interface.PrettyPrint($"Config file already exists: Delete {FileName} to generate it again.", Colors.Fail);
             return;
         }
 
-        // Display a message indicating the generation of the configuration file
         Interface.PrettyPrint($"No config file found, generating {FileName}...", Colors.Info);
 
-        // Check if command-line arguments are provided
+        // Check if any arguments are provided, if not request manual configuration, else try to configure with provided arguments
         if (args.Length == 0)
-            // If no arguments are provided, request configuration settings interactively
             RequestConfiguration();
         else
-            // If command-line arguments are provided, configure settings based on the arguments
             Configure(args);
 
-        // If configuration settings are available, save them to the configuration file
         if (HasConfig)
             Save();
     }
@@ -120,10 +109,7 @@ public class Configuration
             // If no JSON key is specified, retrieve an array of all JSON property names
             string[] jsonPropertyNames = typeof(Configuration).GetProperties().Select(property =>
             {
-                // Get the JSON property name from the JsonPropertyName attribute
                 JsonPropertyNameAttribute jsonPropertyNameAttribute = (JsonPropertyNameAttribute)Attribute.GetCustomAttribute(property, typeof(JsonPropertyNameAttribute))!;
-
-                // Use the JSON property name if available; otherwise, use the property name
                 return jsonPropertyNameAttribute?.Name ?? property.Name;
             }).ToArray();
 
@@ -133,31 +119,22 @@ public class Configuration
         // Find the property that corresponds to the specified JSON key
         PropertyInfo? matchingProperty = typeof(Configuration).GetProperties().FirstOrDefault(property =>
         {
-            // Get the JSON property name from the JsonPropertyName attribute
             JsonPropertyNameAttribute jsonPropertyNameAttribute = (JsonPropertyNameAttribute)Attribute.GetCustomAttribute(property, typeof(JsonPropertyNameAttribute))!;
-
-            // Check if the JSON property name matches the specified key
             return jsonPropertyNameAttribute?.Name == key;
         });
 
         if (matchingProperty is null)
         {
-            // If no matching property is found, print an error message and return an empty array
             Interface.PrettyPrint($"Property with JSON key '{key}' not found.", Colors.Fail);
             return [];
         }
 
-        // If a JSON key is provided without a new value, return the value of the property as the only element of the array
         if (value is null)
             return [matchingProperty!.GetValue(this)!];
 
-        // Convert the new value to the property's type
         object convertedValue = Convert.ChangeType(value, matchingProperty.PropertyType)!;
-
-        // Set the value of the property
         matchingProperty.SetValue(this, convertedValue);
 
-        // Print a message indicating that the property has been set
         Interface.PrettyPrint($"{key} has been set to: {matchingProperty.GetValue(this)}");
         
         return [];
@@ -208,7 +185,6 @@ public class Configuration
         // Pack Format
         while (true)
         {
-            // Retrieve and validate Minecraft version or pack format
             value = MinecraftVersion.GetPackFormat(Interface.GetUserInput("Pack Format or Minecraft version: "));
             if (string.IsNullOrEmpty(value))
                 continue;
@@ -236,7 +212,6 @@ public class Configuration
             break;
         }
 
-        // Create the target file if it doesn't exist
         if (!File.Exists(Target))
         {
             FileStream fs = File.Create(Target);
@@ -253,7 +228,6 @@ public class Configuration
                 break;
             }
 
-            // Validate the output directory path
             if (value.Any(x => x.Equals(Path.GetInvalidPathChars())))
             {
                 Interface.PrettyPrint("Invalid path", Colors.Fail);
@@ -262,11 +236,7 @@ public class Configuration
             break;
         }
 
-        // Create the output directory if it doesn't exist
-        if (!Directory.Exists(value))
-            Directory.CreateDirectory(value);
-
-        // Set the relative path of the output directory
+        Directory.CreateDirectory(value);
         Output = Path.GetRelativePath(value, ".");
     }
 }
