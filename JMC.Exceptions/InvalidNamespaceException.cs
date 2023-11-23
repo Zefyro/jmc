ce3a9e505ead4310ea8c26bbe4b5cb874e30e635
Zefyro <71092443+Zefyro@users.noreply.github.com>
2023-11-23 16:59:56 +0200

    Add docs for exceptions

---
+++ JMC.Exceptions/InvalidNamespaceException.cs
@@ -0,0 +1,6 @@
+﻿namespace JMC.Exceptions;
+
+/// <summary>
+/// Represents an exception thrown when an invalid namespace is encountered in the JMC Compiler.
+/// </summary>
+public class InvalidNamespaceException : Exception { }
--- JMC.Exceptions/RestartException.cs
+++ JMC.Exceptions/RestartException.cs
@@ -1,2 +1,6 @@
 ﻿namespace JMC.Exceptions;
+
+/// <summary>
+/// Represents an exception thrown to signal a restart of the JMC Compiler.
+/// </summary>
 public class RestartException : Exception { }
